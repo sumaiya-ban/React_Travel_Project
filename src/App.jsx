6965e41ced19a250be9f5bcf7b627492aecec311
sumaiya <sumaiyayesmen556@gmail.com>
2025-10-19 09:55:45 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Offers from './components/Offers.jsx';
+import Plan from './components/Plan.jsx';
+import Rooms from './components/Rooms.jsx';
+import ImageSlider from './components/ImageSlider.jsx';
+import Footer from './components/Footer.jsx';
+import './App.css'
+
+export default function App() {
+  return (
+    
+    <>
+    <Navbar/>
+     <Hero/>
+      <Offers/>
+      <Plan/>
+      <Rooms/>
+      <ImageSlider/>
+     <Footer/>
+    </>
+  )
+}
+
+
